ee0130b6ecf7006de6e6f644e762d6d7475d8c25
Luke O'Malley <Lukeomalley95@gmail.com>
2019-06-29 20:20:50 -0400

    Completed Lab 🤙

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  def initialize(input)
+    @input = input.split('').sort
+  end
+
+  def match(potential_anagrams)
+    potential_anagrams.select {|word| word.split('').sort == @input}
+  end
+end
